052b3330d1eec6f43c073226becd0f32035c3dae
harsh <harshpachani@gmail.com>
2022-05-17 22:05:13 +0530

    Initial commit

---
+++ N_17_Boolean.java
@@ -0,0 +1,10 @@
+package com.Udemy;
+
+public class N_17_Boolean {
+    public static void main(String[] args) {
+        boolean bool1 = true;
+        boolean bool2 = false;
+        System.out.println(bool1);
+        System.out.println(bool2);
+    }
+}
---
+++ N_18_BoolComparisons.java
@@ -0,0 +1,26 @@
+package com.Udemy;
+
+public class N_18_BoolComparisons {
+    public static void main(String[] args) {
+
+        int chemistryGrade = 95;
+        int biologyGrade = 75;
+        int englishGrade = 75;
+        System.out.println(chemistryGrade != englishGrade);
+        System.out.println(biologyGrade == englishGrade);
+
+        double sales = 37.55;
+        double cost = 5.55;
+        System.out.println(sales < cost);
+
+        //Comparisons for Strings.
+        //we use equals method to comparison the String
+        //This checks if String values are equal.
+        //NOTE:- Never use == or != to compare String values, you will get weird results.
+        String sentance1 = "I love this course!";
+        String sentance2 = "I love this course!";
+        System.out.println(sentance1.equals(sentance2));
+        System.out.println(!sentance1.equals(sentance2));
+
+    }
+}
