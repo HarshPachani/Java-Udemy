316080716c965c97afcdbd7e749e8509c208b2f5
harsh <harshpachani@gmail.com>
2022-05-17 17:49:30 +0530

    Initial commit

---
+++ N_13_TypeConversion.java
@@ -0,0 +1,11 @@
+package com.Udemy;
+//Typecasting in Java.
+public class N_13_TypeConversion {
+    public static void main(String[] args) {
+        //double to integer.
+        double salary = 5833.333333;
+        int roundSalary = (int)salary;
+        System.out.println("Percy's monthly salary is $"+salary);
+        System.out.println("Percy's monthly salary is $"+roundSalary);
+    }
+}
