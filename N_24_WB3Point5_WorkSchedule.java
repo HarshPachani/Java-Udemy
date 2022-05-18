771ffed5afa55415cc6335ec9977a185aef37b4e
harsh <harshpachani@gmail.com>
2022-05-18 19:57:07 +0530

    Initial commit

---
+++ N_24_WB3Point5_WorkSchedule.java
@@ -0,0 +1,20 @@
+package com.Udemy;
+
+public class N_24_WB3Point5_WorkSchedule {
+    public static void main(String[] args) {
+        // if it's a holiday, print: "woohoo, no work!");
+        // if it's the weekend, print: "it's the weekend, no work!"
+        // otherwise, print: "Wake up at 7:00 :( ";
+
+        int day = 3;
+        boolean holiday = true;
+        if(holiday) {
+            System.out.println("woohoo, no work!");
+        } else if (day == 6 || day == 7) {
+            System.out.println("it's the weekend, no work!");
+        }
+        else{
+            System.out.println("Wake up at 7:00 :( ");
+        }
+    }
+}
