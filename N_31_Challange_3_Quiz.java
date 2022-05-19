334e1deec2032d55bddf9e9482263f4751161729
harsh <harshpachani@gmail.com>
2022-05-19 21:34:19 +0530

    Initial commit

--- N_31_Challange_3_Quiz.java
+++ N_31_Challange_3_Quiz.java
@@ -9,22 +9,22 @@ public class N_31_Challange_3_Quiz {
         System.out.println("1. Which country held the 2016 Summer Olympics?");
         System.out.println("\ta) China\n\tb) Ireland\n\tc) Brazil\n\td) Italy\n");
         //store answer 1
-        String ans1 = scan.next();
+        String ans1 = scan.nextLine();
 
         System.out.println("\n2. Which planet is the hottest?");
         System.out.println("\ta) Venus\n\tb) Saturn\n\tc) Mercury\n\td) Mars\n");
         //store answer 2
-        String ans2 = scan.next();
+        String ans2 = scan.nextLine();
 
         System.out.println("\n3. What is the rarest blood type?");
         System.out.println("\ta) O\n\tb) A\n\tc) B\n\td) AB-Negative\n");
         //store answer 3
-        String ans3 = scan.next();
+        String ans3 = scan.nextLine();
 
         System.out.println("\n4. Which one of these characters is friends with Harry Potter?");
         System.out.println("\ta) Ron Weasley\n\tb) Hermione Granger\n\tc) Draco Malfoy\n");
         //store answer 4
-        String ans4 = scan.next();
+        String ans4 = scan.nextLine();
 
         int score = 0;
         //Task 2: Check each answer - For each correct answer, add 5 points to the score.
