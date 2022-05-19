17f19f3eb948c1846ced35124870b5a8ed769c42
harsh <harshpachani@gmail.com>
2022-05-19 22:05:26 +0530

    Initial commit

---
+++ N_32_Chorus.java
@@ -0,0 +1,19 @@
+package com.Udemy;
+//Intro of the Function.
+
+public class N_32_Chorus {
+    public static void main(String[] args) {
+        singChorus();
+        singChorus();
+        singChorus();
+    }
+
+    //There is static in this singChorus function because we are calling this function
+    // from main method which is as we know static, so we have to write static in the function.
+    public static void singChorus(){
+        System.out.println("Don't blame it on the sunshine");
+        System.out.println("Don't blame it on moonlight");
+        System.out.println("Don't blame it on good times");
+        System.out.println("Blame it on the boogie\n");
+    }
+}
