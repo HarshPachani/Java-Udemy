ba11f9defd9aabf5cd87152882c4978390ee6db3
harsh <harshpachani@gmail.com>
2022-05-28 11:19:22 +0530

    Initial commit

---
+++ Module_1/Function_4/N_42_Challange_4_RockPaperScissors.java
@@ -0,0 +1,130 @@
+package com.Udemy.Module_1.Function_4;
+
+import java.util.Scanner;
+
+public class N_42_Challange_4_RockPaperScissors {
+    public static String main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Let's play Rock Paper Scissors.");
+        System.out.println("When I say 'shoot', Choose: rock, paper, or scissors.\n");
+        System.out.println("Are you ready? Write 'yes' if you are.");
+
+        //Task 1: See if the user wants to play.
+        String answer = scan.nextLine();
+
+    /*Task 2: Set up the game
+       • if the answer is yes:
+             – print: Great!
+             – print: rock – paper – scissors, shoot!
+             – pick up user's choice.
+             – get the computer choice (can only be done after task 3).
+             – get the result (can only be done after task 4)
+             – print everything (can only be done after task 5).
+       • else:
+             – print: Darn, some other time...!
+    */
+    if (answer.equals("yes")){
+        System.out.println("Great!");
+        System.out.println("rock – paper – scissors, shoot!");
+        String choice = scan.nextLine();
+        computerChoice();
+        scan.close();
+    }
+    else {
+        System.out.println("Darn, some other time...!");
+    }
+
+    //Task 3  – Write a function where the computer picks a random choice.
+    /**
+     * Function name: computerChoice - picks randomly between rock paper and scissors
+     * @return a choice (String).
+     *
+     * Inside the function:
+     *   1. Picks a random number between 0 and 2.
+     *   2. if 0: returns the choice 'rock'
+     *      if 1: returns the choice 'paper'
+     *      if 2: returns the choice 'scissors'
+     */
+    public static String computerChoice(){
+        double randomNumber = Math.random() * 3;
+        int num = (int)randomNumber;
+
+        if (num == 0){
+            return "rock";
+        }
+        else if(num == 1){
+            return "paper";
+        } else if (num == 2) {
+            return "scissors";
+        }
+    }
+
+
+
+    //Task 4  – Write a function that compares the choices and returns the result.
+
+    /**
+     * Function name: result - It returns the result of the game.
+     *  @param yourChoice (String)
+     *  @param computerChoice (String)
+     *  @return result (String)
+
+     * Inside the function:
+     *   1. result is "You win" if:
+     *
+     *       You: "rock"      Computer: "scissors"
+     *       You: "paper"     Computer: "rock"
+     *       You: "scissors"  Computer: "paper"
+     *
+     *   2. result is "You lose" if:
+     *
+     *       Computer: "rock"      You: "scissors"
+     *       Computer: "paper"     You: "rock"
+     *       Computer: "scissors"  You: "paper"
+     *
+     *   3. result is "It's a tie" if:
+     *
+     *       your choice equals computer choice.
+     *
+     */
+
+    public static String result(String yourChoice, String computerChoice) {
+
+        if(yourChoice.equals("rock") && computerChoice.equals("scissors") || yourChoice.equals("paper") && computerChoice.equals("rock") || yourChoice.equals("scissors") && computerChoice.equals("paper"))
+        {
+            String ans = "You Win!";
+        }
+
+        else if(yourChoice.equals("scissors") && computerChoice.equals("rock") || yourChoice.equals("rock") && computerChoice.equals("paper") || yourChoice.equals("paper") && computerChoice.equals("scissors"))
+            {
+                String ans = "You Lose!";
+            }
+        else {
+            String ans = "It's a tie!";
+        }
+        return ans;
+    }
+
+    //Task 5  – Write a function that prints your choice, the computer's, and the result.
+
+    /**
+     * Name: printResult - It prints everything (your choice, computer choice, result)
+     * @param yourChoice (String)
+     * @param computerChoice (String)
+     * @param result (String)
+     *
+     * Inside the function:
+     *
+     *  1. prints everything:
+     *      – prints: You chose:          <your choice>
+     *      – prints: The computer chose: <computer choice>
+     *      – prints: <result>
+     */
+    public static void printResult(String yourChoice,String computerChoice,String ans){
+            System.out.println("You chose: "+yourChoice);
+            System.out.println("The computer chose: "+computerChoice);
+            System.out.println(ans);
+    }
+  }
+}
