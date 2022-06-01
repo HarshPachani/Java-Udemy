b21804ff78635c81e0330f93317648c12dc89bc6
harsh <harshpachani@gmail.com>
2022-06-01 21:34:16 +0530

    Initial commit

---
+++ Module_1/Arrays_6/N_1_IntroToArrays.java
@@ -0,0 +1,12 @@
+package com.Udemy.Module_1.Arrays_6;
+
+public class N_1_IntroToArrays {
+    public static void main(String[] args) {
+        String[] kingdoms = {"Mercia" , "Wessex" , "Northnumbria" , "E A"};
+        System.out.println(kingdoms);
+        System.out.println(kingdoms[0]);
+        System.out.println(kingdoms[1]);
+        System.out.println(kingdoms[2]);
+        System.out.println(kingdoms[3]);
+    }
+}
