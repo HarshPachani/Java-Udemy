93f0619938e744fd0bab23d74b4f854389ad24fa
Harsh <harshpachani@gmail.com>
2022-07-11 10:15:26 +0530

    Added comment

--- Module_2/The_WorldOf_Objects_07/N_02_WB7Point1_AirlineReservationSystem/Person_N_02.java
+++ Module_2/The_WorldOf_Objects_07/N_02_WB7Point1_AirlineReservationSystem/Person_N_02.java
@@ -1,5 +1,6 @@
 package com.Udemy.Module_2.The_WorldOf_Objects_07.N_02_WB7Point1_AirlineReservationSystem;
 
+//This is the bluePrint of the Reservation system.
 public class Person_N_02 {
     String name;
     String nationality;
