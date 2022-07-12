8b2609654185208a84aa65cd919e7cf169650eea
harsh pachani <98544999+HarshPachani@users.noreply.github.com>
2022-07-12 22:33:53 +0530

    Update Dealership.java

--- Module_2/The_WorldOf_Objects_07/Dealership.java
+++ Module_2/The_WorldOf_Objects_07/Dealership.java
@@ -3,11 +3,11 @@ package com.Udemy.Module_2.The_WorldOf_Objects_07;
 //The Manager class manages objects of another class.
 /*
 Requirements:-
-  .The dealership sells many types of cars.
-  .A car is defined by it's make, price, year and color.
-  .Every car needs to come with square parts.
-  .The dealership app should be able to search for cars based on make and price.
-  .After a sale, the car must drive to the nearest exit.
+  •The dealership sells many types of cars.
+  •A car is defined by it's make, price, year and color.
+  •Every car needs to come with square parts.
+  •The dealership app should be able to search for cars based on make and price.
+  •After a sale, the car must drive to the nearest exit.
 */
 
 public class Dealership {
