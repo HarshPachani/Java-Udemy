1c9bb8b1b808991657622025350463655d645b47
Harsh <harshpachani@gmail.com>
2022-07-15 11:23:15 +0530

    Initial commit

---
+++ Module_2/The_WorldOf_Objects_07/Workbook_02_VendingMachineApp/Item.java
@@ -0,0 +1,5 @@
+package com.Udemy.Module_2.The_WorldOf_Objects_07.Workbook_02_VendingMachineApp;
+
+public class Item {
+
+}
---
+++ Module_2/The_WorldOf_Objects_07/Workbook_02_VendingMachineApp/Machine.java
@@ -0,0 +1,15 @@
+package com.Udemy.Module_2.The_WorldOf_Objects_07.Workbook_02_VendingMachineApp;
+
+public class Machine {
+        /**
+         * Function name – dispense
+         * @param row (int)
+         * @param spot (int)
+         * @return (boolean)
+         *
+         * Inside the function:
+         *  1. Checks if the requested item has a quantity bigger than 0.
+         *      • if so: decreases its quantity by one and returns true.
+         *      • otherwise: returns false.
+         */
+}
