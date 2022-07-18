87e82d3eaffd0b7fe3b71484e642819f2e44d900
Harsh <harshpachani@gmail.com>
2022-07-18 11:58:55 +0530

    Initial commit

---
+++ Module_2/ExceptionHandling_and_Debugging_08/DebuggingLessons/Intro_to_Debugging/Conditionals.java
@@ -0,0 +1,23 @@
+package com.Udemy.Module_2.ExceptionHandling_and_Debugging_08.DebuggingLessons.Intro_to_Debugging;
+
+public class Conditionals {
+        public static void main(String[] args) {
+            String option = "";
+
+            int random = (int) Math.random() * 1 + 3;
+
+            switch (random) {
+                case 1: option = "hit";
+                case 2: option = "stay";
+                case 3: option = "fold";
+                default: option = "This should never get called";
+            }
+
+
+            if (!option.equals("hit") || !option.equals("stay")) {
+                System.out.println("fold");
+            } else {
+                System.out.println(option);
+            }
+        }
+}
