01e277d9545048ebe454838b6a25391c64c7f15e
Harsh <harshpachani@gmail.com>
2022-07-18 11:50:48 +0530

    Initial commit

---
+++ Module_2/ExceptionHandling_and_Debugging_08/DebuggingLessons/Breakpoints.java
@@ -0,0 +1,12 @@
+package com.Udemy.Module_2.ExceptionHandling_and_Debugging_08.DebuggingLessons;
+
+public class Breakpoints {
+        public static void main(String[] args) {
+            int number = 5;
+            long bigNumber = 50000000000L;
+            double decimal = 5.5;
+            boolean flag = true;
+            char letter = 'f';
+            String word = "hello";
+        }
+}
