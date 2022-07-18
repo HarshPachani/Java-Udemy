8bb33f948d368808f34e91748e68f4a183efe93a
Harsh <harshpachani@gmail.com>
2022-07-18 19:59:40 +0530

    Initial commit

--- Module_2/ExceptionHandling_and_Debugging_08/Exception_Handling_Lessons/Checked_Exceptions/ReadingFiles_01.java
+++ Module_2/ExceptionHandling_and_Debugging_08/Exception_Handling_Lessons/Checked_Exceptions/ReadingFiles_01.java
@@ -1,5 +1,10 @@
 package com.Udemy.Module_2.ExceptionHandling_and_Debugging_08.Exception_Handling_Lessons.Checked_Exceptions;
 
+import java.io.FileInputStream;
+
 public class ReadingFiles_01 {
-    
+    public static void main(String[] args) throws FileNotFoundException {
+        FileInputStream fis = new FileInputStream("Greetings.txt");
+
+    }
 }
