890b0be2b9323edbc71ac6592f3a8d94d07eaf73
Harsh <harshpachani@gmail.com>
2022-07-19 11:35:17 +0530

    Initial commit

--- Module_2/ExceptionHandling_and_Debugging_08/Exception_Handling_Lessons/Throwing_UncheckedExceptions_03/Main.java
+++ Module_2/ExceptionHandling_and_Debugging_08/Exception_Handling_Lessons/Throwing_UncheckedExceptions_03/Main.java
@@ -4,6 +4,15 @@ package com.Udemy.Module_2.ExceptionHandling_and_Debugging_08.Exception_Handling
 import com.Udemy.Module_2.ExceptionHandling_and_Debugging_08.Exception_Handling_Lessons.Throwing_UncheckedExceptions_03.Models.Employee;
 import com.Udemy.Module_2.ExceptionHandling_and_Debugging_08.Exception_Handling_Lessons.Throwing_UncheckedExceptions_03.Models.Store;
 
+//Why do we throw an unchecked exceptions?
+/*
+    ->  We throw unchecked exceptions that prevent the caller from misusing methods or constructors.
+*/
+
+/*The Most common exceptions to throw:
+    -> IllegalArgumentException.
+    -> IllegalStateException.
+*/
 public class Main {
         public static void main(String[] args) {
             Employee stocker = new Employee("   ", null);
--- Module_2/ExceptionHandling_and_Debugging_08/Exception_Handling_Lessons/Throwing_UncheckedExceptions_03/Models/Employee.java
+++ Module_2/ExceptionHandling_and_Debugging_08/Exception_Handling_Lessons/Throwing_UncheckedExceptions_03/Models/Employee.java
@@ -5,6 +5,9 @@ public class Employee {
         private String position;
 
         public Employee(String name, String position) {
+            if (name == null || name.isBlank() || position == null || position.isBlank()){
+                throw new IllegalArgumentException("Name or position cannot be null/blank");
+            }
             this.name = name;
             this.position = position;
         }
