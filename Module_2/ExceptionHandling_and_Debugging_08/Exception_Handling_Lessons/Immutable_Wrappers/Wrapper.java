ca23cafe4991957160aed1abb4d3d35c4c6f9a5c
Harsh <harshpachani@gmail.com>
2022-07-24 16:57:40 +0530

    Initial commit

---
+++ Module_2/ExceptionHandling_and_Debugging_08/Exception_Handling_Lessons/Immutable_Wrappers/Wrapper.java
@@ -0,0 +1,11 @@
+package com.Udemy.Module_2.ExceptionHandling_and_Debugging_08.Exception_Handling_Lessons.Immutable_Wrappers;
+
+public class Wrapper {
+    public static void main(String[] args) {
+        int wholeNumber = 5;
+        long largeWholeNumber = 5000000000L;
+        double decimal = 5.493;
+        char letter = 'b';
+    }
+}
+
