1362a4e456ff6d5d9b371f2e0566f8d325830ca1
Harsh <harshpachani@gmail.com>
2022-07-24 17:11:00 +0530

    Initial commit

--- Module_2/ExceptionHandling_and_Debugging_08/Exception_Handling_Lessons/Immutable_Wrappers/Wrapper.java
+++ Module_2/ExceptionHandling_and_Debugging_08/Exception_Handling_Lessons/Immutable_Wrappers/Wrapper.java
@@ -1,9 +1,11 @@
 package com.Udemy.Module_2.ExceptionHandling_and_Debugging_08.Exception_Handling_Lessons.Immutable_Wrappers;
+
 //Wrapper:- A Wrapper is an immutable class that wraps around a primitive type.
 //Ex:- Integer  -> Immutable class that wraps around int.
 //          OR
 /* Integer is an immutable class that wraps around the type because integer is an immutable class, then variables of that type store a reference to an object.
 They can be null, they can call methods, but they cannot change. which means they are immune to the reference trap.*/
+
 public class Wrapper {
     public static void main(String[] args) {
         //int wholeNumber = 5;
